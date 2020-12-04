d1dfbcb05dae17352063ffe84acfc020322f66da
Kyle Eriss <kyle.eriss@gmail.com>
2020-12-04 00:24:24 -0500

    proptypes revisions

---
+++ src/ValidateError.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function ValidationError(props){
+    if(props.message){
+        return(
+            <div>{props.message}</div>
+        )
+    }
+    return <></>
+}
+
+ValidationError.propTypes = {
+    message: PropTypes.string
+}
